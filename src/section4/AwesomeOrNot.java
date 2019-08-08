7291b0fdad10f5871e7abb2c4140ef549e4aa5f4
League Student <league@iMac-5.attlocal.net>
2019-08-08 14:23:06 -0700

    Eclipse#3

--- src/section4/AwesomeOrNot.java
+++ src/section4/AwesomeOrNot.java
@@ -2,32 +2,44 @@ package section4;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes all the steps below…
+public static void main(String[] args) {
+	
 
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
+int rnum = new Random().nextInt(4);
 	// 3. Print out this variable
-
+JOptionPane.showMessageDialog(null, rnum);
 	// 4. Get the user to enter something that they think is awesome
-
+JOptionPane.showInputDialog("What do you think is awesome?");
 	// 5. If the random number is 0
-
+if( rnum==0 ) {
+	JOptionPane.showMessageDialog(null, "Awesome!");
+	}
 	// -- tell the user whatever they entered is awesome!
-
+if( rnum==1 ) {
+	JOptionPane.showMessageDialog(null, "Ok.");
+	}
 	// 6. If the random number is 1
 
 	// -- tell the user whatever they entered is ok.
 
 	// 7. If the random number is 2
-
+if( rnum==2 ) {
+	JOptionPane.showMessageDialog(null, "Booorrrinngggg!!!");
+	}
 	// -- tell the user whatever they entered is boring.
 
 	// 8. If the random number is 3
 
 	// -- write your own answer
-
+if( rnum==3 ) {
+	JOptionPane.showMessageDialog(null, "( ͠° ͟ʖ ͡°)");
+	}
 }
-
+}
 
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,20 +1,55 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+		int score=1;
 		// 2.  Ask the user a question 
-		
+		String answer=JOptionPane.showInputDialog("How many fingers does a normal human have?");
 		// 3.  Use an if statement to check if their answer is correct
-		
+		if(answer.equalsIgnoreCase("Ten.")) {
+			JOptionPane.showMessageDialog(null, "Correct.");
+			}else {
+				JOptionPane.showMessageDialog(null, "Wrong.");
+			}
 		// 4.  if the user's answer was correct, add one to their score 
 		
+			if(answer.equalsIgnoreCase("Ten.")) {
+				score+= 1;
+				
+		}
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
+		String answer1=JOptionPane.showInputDialog("How many faces does a normal human have?");
+		// 3.  Use an if statement to check if their answer is correct
+		if(answer1.equalsIgnoreCase("One.")) {
+			JOptionPane.showMessageDialog(null, "Correct.");
+			}else {
+				JOptionPane.showMessageDialog(null, "Wrong.");
+			}
+		// 4.  if the user's answer was correct, add one to their score 
 		
+			if(answer1.equalsIgnoreCase("One.")) {
+				score+= 1;
+				
+			String answer2=JOptionPane.showInputDialog("How many ears does a normal human have?");
+			// 3.  Use an if statement to check if their answer is correct
+			if(answer2.equalsIgnoreCase("Two.")) {
+				JOptionPane.showMessageDialog(null, "Correct.");
+				}else {
+					JOptionPane.showMessageDialog(null, "Wrong.");
+				}
+			// 4.  if the user's answer was correct, add one to their score 
+			
+				if(answer1.equalsIgnoreCase("Two.")) {
+					score+= 1;
+					}
+		}
 		// 6.  After all the questions have been asked, print the user's score 
-		
+		JOptionPane.showMessageDialog(null, score);
 	}
-}
+	}
+
